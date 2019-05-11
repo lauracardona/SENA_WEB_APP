9854c852a8761bc301a938c5cd93c0a958ffbc42
laura cardona <lcardona57@misena.edu.co>
2019-05-11 10:06:15 -0500

    active_record

---
+++ app/models/active_record/base.rb
@@ -0,0 +1,26 @@
+module Storage
+    class Base 
+        def initialize(params = {})
+            params.each do   |key,value|
+            instance_variable_set("@#{key}",value) #Metodo que toma una variable de instacia y la crea con su valor
+                self.class.send(:define_method,key) do #:define_method crea un motodo para traer los valores
+                    instance_variable_get("@#{key}")
+                end
+            end
+        end
+
+
+#Metodo para escribir sobre el archivo
+        def save
+           
+        end   
+
+        def self.all
+        end
+  
+#Metodo para cargar  un archivo de texto
+         def self.tableName
+            raise("NotImplementedError")
+        end
+    end
+end
---
+++ app/models/active_record/client.rb
@@ -0,0 +1,9 @@
+require 'pg'
+module ActiveRecord
+
+    class Client
+        def initialize
+            
+        end
+    end
+end
